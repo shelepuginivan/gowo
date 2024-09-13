ecabab7acf222be8122d856d8275287b9733b3d0
shelepuginivan <shelepuginivanm@gmail.com>
2024-09-13 22:03:04 +0300

    feat: add owofication variables

---
+++ var.go
@@ -0,0 +1,46 @@
+package gowo
+
+// List of actions used during text owofication.
+// Can be modified.
+var Actions = []string{
+	"*blushes*",
+	"*giggles*",
+	"*smiles*",
+}
+
+// List of emoticons used during text owofication.
+// Can be modified.
+var Emoticons = []string{
+	"≧◠‿◠≦",
+	"(✿◠‿◠)",
+	"≧◉◡◉≦",
+	"☜(ˆ▿ˆc)",
+	"٩(˘◡˘)۶",
+	"(̶◉͛‿◉̶)",
+	"(╥﹏╥)",
+	"(*≧▽≦)",
+	"(・`ω´・)",
+	":3",
+	"> ~ <",
+}
+
+// Map of string replaces replacements used during text owofication.
+// Can be modified.
+var Replacements = map[string]string{
+	"l":  "w",
+	"r":  "w",
+	"L":  "W",
+	"R":  "W",
+	"oo": "uwu",
+	"e":  "we",
+	"na": "nya",
+	"ne": "nye",
+	"ni": "nyi",
+	"no": "nyo",
+	"nu": "nyu",
+	"Na": "Nya",
+	"Ne": "Nye",
+	"Ni": "Nyi",
+	"No": "Nyo",
+	"Nu": "Nyu",
+}
