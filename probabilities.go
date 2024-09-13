ae142b6a62a56c0477a0444e3180dd1f644252c3
shelepuginivan <shelepuginivanm@gmail.com>
2024-09-13 22:53:03 +0300

    feat: add default probabilities

---
+++ probabilities.go
@@ -0,0 +1,9 @@
+package gowo
+
+// Default probabilities.
+const (
+	ProbabilityOfAction      = 0.05
+	ProbabilityOfEmoticon    = 0.05
+	ProbabilityOfReplacement = 1.00
+	ProbabilityOfStutter     = 0.10
+)
