16c0690daf27fbee74c326597ab81007291d438b
shelepuginivan <shelepuginivanm@gmail.com>
2024-09-14 21:23:35 +0300

    feat: implement the `Owoify` method

--- owoifier.go
+++ owoifier.go
@@ -1,10 +1,31 @@
 package gowo
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+// makeReplacer creates an instance of [strings.Replacer] with map m used as
+// oldnew.
+func makeReplacer(m map[string]string) *strings.Replacer {
+	oldnew := make([]string, 2*len(m))
+	i := 0
+
+	for k, v := range m {
+		oldnew[i] = k
+		oldnew[i+1] = v
+		i += 2
+	}
+
+	return strings.NewReplacer(oldnew...)
+}
+
 // Owoifier is a struct that performs text owofication.
 type Owoifier struct {
-	actions      []string          // Actions slice.
-	emoticons    []string          // Emoticons slice.
-	replacements map[string]string // Word replacements.
+	actions   []string          // Actions slice.
+	emoticons []string          // Emoticons slice.
+	replacer  *strings.Replacer // Token replacer.
 
 	probabilityOfAction      float64 // Probability of action occurrance.
 	probabilityOfEmoticon    float64 // Probability of emoticon occurrance.
@@ -17,7 +38,7 @@ func New() *Owoifier {
 	return &Owoifier{
 		actions:                  Actions,
 		emoticons:                Emoticons,
-		replacements:             Replacements,
+		replacer:                 makeReplacer(Replacements),
 		probabilityOfAction:      ProbabilityOfAction,
 		probabilityOfEmoticon:    ProbabilityOfEmoticon,
 		probabilityOfReplacement: ProbabilityOfReplacement,
@@ -35,7 +56,7 @@ func NewWithProbabilities(
 	return &Owoifier{
 		actions:                  Actions,
 		emoticons:                Emoticons,
-		replacements:             Replacements,
+		replacer:                 makeReplacer(Replacements),
 		probabilityOfAction:      probabilityOfAction,
 		probabilityOfEmoticon:    probabilityOfEmoticon,
 		probabilityOfReplacement: probabilityOfReplacement,
@@ -52,7 +73,7 @@ func NewWithVars(
 	return &Owoifier{
 		actions:                  actions,
 		emoticons:                emoticons,
-		replacements:             replacements,
+		replacer:                 makeReplacer(replacements),
 		probabilityOfAction:      ProbabilityOfAction,
 		probabilityOfEmoticon:    ProbabilityOfEmoticon,
 		probabilityOfReplacement: ProbabilityOfReplacement,
@@ -74,10 +95,56 @@ func NewWithVarsProbabilities(
 	return &Owoifier{
 		actions:                  actions,
 		emoticons:                emoticons,
-		replacements:             replacements,
+		replacer:                 makeReplacer(replacements),
 		probabilityOfAction:      probabilityOfAction,
 		probabilityOfEmoticon:    probabilityOfEmoticon,
 		probabilityOfReplacement: probabilityOfReplacement,
 		probabilityOfStutter:     probabilityOfStutter,
 	}
+}
+
+// Owoify performs text owofication:
+//   - Replaces symbols and substrings with replacements
+//   - Adds s-stutters
+//   - Adds actions *blushes*
+//   - Adds emoticons ≧◉◡◉≦
+func (o Owoifier) Owoify(text string) string {
+	tokens := strings.Split(text, " ")
+	result := make([]string, 0, len(tokens))
+
+	for _, token := range tokens {
+		newToken := token
+
+		if rand.Float64() < o.probabilityOfReplacement {
+			newToken = o.replacer.Replace(token)
+		}
+
+		if rand.Float64() < o.probabilityOfStutter {
+			newToken = fmt.Sprintf("%c-%s", newToken[0], newToken)
+		}
+
+		result = append(result, newToken)
+
+		if rand.Float64() < o.probabilityOfAction {
+			result = append(result, o.RandomAction())
+		}
+
+		if rand.Float64() < o.probabilityOfEmoticon {
+			result = append(result, o.RandomEmoticon())
+		}
+	}
+
+	return strings.Join(result, " ")
+}
+
+// RandomAction returns a random action of the Owoifier.
+func (o Owoifier) RandomAction() string {
+	i := rand.Intn(len(o.actions))
+	return o.actions[i]
+}
+
+// RandomEmoticon returns a random emoticon of the Owoifier.
+func (o Owoifier) RandomEmoticon() string {
+	i := rand.Intn(len(o.emoticons))
+	return o.emoticons[i]
 }
